2f2ee7e342bae8af22ca07375d5d479fb0ed52ec
sunsplat <heeycutie@yahoo.com>
2016-01-14 16:27:18 -0500

    adding variable

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -5,15 +5,16 @@ def bubble_sort(array)
     
     is_swap =  false
 
-    (0...array.size-1).each do |index|
+    num = array.size-1
+    (0...num).each do |index|
       if array[index] > array[index+1]
          array[index],array[index+1] = array[index+1],array[index]
+
          is_swap = true
       end 
-    end
-    
+    end 
     break if !is_swap 
-  
+    num -= 1
   end
 
   array
